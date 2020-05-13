de71536f160224af02a17b14da052c659392894f
snehalkale41995 <kalesnehal1007@gmail.com>
2020-05-14 00:01:56 +0530

    added ingredients

--- src/app/pages/meal-detail/meal-detail.page.ts
+++ src/app/pages/meal-detail/meal-detail.page.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { DeliveryData } from '../../providers/delivery-data';
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-meal-detail',
   templateUrl: './meal-detail.page.html',
   styleUrls: ['./meal-detail.page.scss'],
 })
-export class MealDetailPage implements OnInit {
+export class MealDetailPage {
+  defaultHref = ''
+  mealDetails: any;
+  ingredientList : any;
+  restaurantDetails : any;
+  constructor(
+    private dataProvider: DeliveryData,
+    private route: ActivatedRoute,
+    public actionSheetCtrl: ActionSheetController,
+    public confData: DeliveryData,
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.dataProvider.load().subscribe((data: any) => {
+      const mealId = this.route.snapshot.paramMap.get('mealId');
+      if (data && data.restaurantList) {
+        for (const item of data.restaurantList) {
+          let mealList = item.menuList
+        {
+          for(const meal of mealList) {
+          if (meal && meal.$key === mealId) {
+            this.mealDetails = meal;
+            this.restaurantDetails = item
+            }
+         }
+      }
+    }
+  }
+   this.ingredientList = this.mealDetails.ingredients
+   console.log("this.ingredientList", this.ingredientList)
+    });
   }
 
 }
