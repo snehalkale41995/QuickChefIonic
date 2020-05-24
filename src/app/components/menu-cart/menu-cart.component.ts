b20c987526ab7b3f8416200ad2d2507c68cb1790
snehalkale41995 <kalesnehal1007@gmail.com>
2020-05-24 20:40:40 +0530

    added count function

--- src/app/components/menu-cart/menu-cart.component.ts
+++ src/app/components/menu-cart/menu-cart.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./menu-cart.component.scss'],
 })
 export class MenuCartComponent implements OnInit {
+ 
+  @Input() menuList : []
 
-  @Input() MenuDetails : {}
+  countBtnActn = 'add' ;
   constructor() {}
 
   ngOnInit() {}
+
+
+   countAction(action, id){
+    console.log("id", id)
+    this.menuList.forEach((menu:any)=>{
+    //  console.log("menu", menu.id)
+      if(menu && menu.id == id){
+        if(action==='add')
+         menu.count = menu.count + 1;
+         else
+         menu.count = menu.count - 1;
+      }
+      })
+  }
 
 }
