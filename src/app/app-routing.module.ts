26967bea5a1a56239817a794d3f29f2656d5fa96
snehalkale41995 <kalesnehal1007@gmail.com>
2020-05-28 18:32:02 +0530

    login and signup pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,54 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { InitialPagePage } from './pages/initial-page/initial-page.page';
-
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { InitialPagePage } from "./pages/initial-page/initial-page.page";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: '/initial-page',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "/initial-page",
+    pathMatch: "full",
   },
   {
-    path: 'app',
-    loadChildren: () => import('./pages/tabs-page/tabs-page.module').then(m => m.TabsModule)
+    path: "app",
+    loadChildren: () =>
+      import("./pages/tabs-page/tabs-page.module").then((m) => m.TabsModule),
   },
   {
-    path: 'landing',
-    loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule),
+    path: "landing",
+    loadChildren: () =>
+      import("./pages/landing/landing.module").then((m) => m.LandingModule),
   },
-  { path: 'initial-page',
-   loadChildren: () => import('./pages/initial-page/initial-page.module').then(m => m.InitialPagePageModule),
+  {
+    path: "initial-page",
+    loadChildren: () =>
+      import("./pages/initial-page/initial-page.module").then(
+        (m) => m.InitialPagePageModule
+      ),
   },
-  { path: 'user-profile', loadChildren: './pages/user-profile/user-profile.module#UserProfilePageModule' },
-  { path: 'map',
-   loadChildren: () => import('./pages/map/map.module').then(m => m.MapModule),
+  {
+    path: "user-profile",
+    loadChildren:
+      "./pages/user-profile/user-profile.module#UserProfilePageModule",
+  },
+  {
+    path: "map",
+    loadChildren: () =>
+      import("./pages/map/map.module").then((m) => m.MapModule),
+  },
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./pages/login/login.module").then((m) => m.LoginModule),
+  },
+  {
+    path: "signup",
+    loadChildren: () =>
+      import("./pages/signup/signup.module").then((m) => m.SignUpModule),
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
